56d315d98c03539eb5fc098e6be7ea34a2517fb8
thereatiy <wantwedead@gmail.com>
2024-06-08 23:13:46 +0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -486,7 +486,7 @@ function generatePassiveAbility(roleName) {
 let currentPart = 0;
 let currentPlayer = 1;
 const totalPlayers = 4;
-const apiKey = 'YOUR_API_KEY'; // Замените YOUR_API_KEY на ваш ключ API
+const apiKey = 'AIzaSyAm_elTp1qDKPHTZf_KmTw9eucaDGgu_xw'; // Замените YOUR_API_KEY на ваш ключ API
 
 // Функция для обновления текста истории и генерации изображения
 async function updateStory() {
